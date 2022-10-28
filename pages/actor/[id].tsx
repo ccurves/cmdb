9623b648723391372688c35f8d19b03561681199
ccurves <simoncovenant47@gmail.com>
2022-10-28 12:31:23 +0100

    initial commit

---
+++ pages/actor/[id].tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+type Props = {};
+
+const Actors: NextPage<Props> = (props: Props) => {
+  return <div>[id]</div>;
+};
+
+export default Actors;
